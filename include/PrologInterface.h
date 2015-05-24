19a2443905b639611f2f6ea4248b828958445d4c
Henrique Nazaré <henry.nazare@gmail.com>
2015-05-24 11:01:25 -0300

    Add PrologTerm and PrologTermHolder classes

--- include/PrologInterface.h
+++ include/PrologInterface.h
@@ -2,9 +2,38 @@
 
 #include <SWI-Prolog.h>
 
+#include <ostream>
+
 class PrologLifetime {
 public:
   static void begin(int argc, char *argv[]);
   static void end();
+};
+
+class PrologTermHolder {
+public:
+  PrologTermHolder(term_t term);
+
+  term_t getInternalTerm() const;
+
+protected:
+   term_t term_;
+};
+
+class PrologTerm : public PrologTermHolder {
+  friend std::ostream &operator<<(
+      std::ostream &os, const PrologTerm &printable);
+
+public:
+  typedef std::function<void (std::ostream&, term_t)> PrintFnTy;
+
+  void print(std::ostream &os) const;
+
+protected:
+  PrologTerm(PrintFnTy printFn);
+  PrologTerm(term_t term, PrintFnTy printFn);
+
+private:
+  PrintFnTy printer_;
 };
 
--- src/PrologInterface.cpp
+++ src/PrologInterface.cpp
@@ -1,5 +1,13 @@
 #include "PrologInterface.h"
 
+std::ostream &operator<<(std::ostream &os, const PrologTerm &term) {
+  term.print(os);
+  return os;
+}
+
+/**
+ * PrologLifetime
+ */
 void PrologLifetime::begin(int argc, char *argv[]) {
   if (!PL_initialise(argc, argv)) {
     PL_halt(EXIT_FAILURE);
@@ -8,5 +16,30 @@ void PrologLifetime::begin(int argc, char *argv[]) {
 
 void PrologLifetime::end() {
   PL_cleanup(EXIT_SUCCESS);
+}
+
+/**
+ * PrologTermHolder
+ */
+PrologTermHolder::PrologTermHolder(term_t term) : term_(term) {
+}
+
+term_t PrologTermHolder::getInternalTerm() const {
+  return term_;
+}
+
+/**
+ * PrologTerm
+ */
+PrologTerm::PrologTerm(PrintFnTy printFn)
+    : PrologTerm(PL_new_term_ref(), printFn) {
+}
+
+PrologTerm::PrologTerm(term_t term, PrintFnTy printFn)
+    : PrologTermHolder(term), printer_(printFn) {
+}
+
+void PrologTerm::print(std::ostream &os) const {
+  printer_(os, term_);
 }
 
